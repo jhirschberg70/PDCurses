4c84db28006a572d9583b3bb288baa9562432960
Jeff Hirschberg <jeff@deepmanlyvoice.com>
2026-05-15 14:01:15 -0700

    Refactor PDC_transform_line()
    This commit refactors PDC_transform_line() to create a line buffer and
    then make a single call to JavaScript to transform the line. The
    previous implementation called PDCurses.setCell() for each cell in the
    line being transformed, which was inefficient. On the JavaScript side,
    I've left setCell() in place and just call it from inside the new
    PDCurses.transform_line() function. Essentially, I've just shifted the
    calling of setCell() from C to JavaScript to avoid having to cross the
    C/JavaScript boundary for each cell in the line. This should be a slight
    improvement in performance when transforming lines, especially for
    longer lines.

--- emscripten/pdcdisp.c
+++ emscripten/pdcdisp.c
@@ -8,7 +8,6 @@ void PDC_doupdate(void)
     PDC_LOG(("PDC_doupdate() - called"));
 }
 
-
 /* draw a cursor at (y, x) */
 
 void PDC_gotoyx(int row, int col)
@@ -26,14 +25,29 @@ void PDC_transform_line(int lineno, int x, int len, const chtype *srcp)
 {
     PDC_LOG(("PDC_transform_line() - called: lineno=%d\n", lineno));
 
+    static PDC_EMSCRIPTEN_CELL *line_buffer = NULL;
+    static int line_buffer_size = 0;
+
+    if (!line_buffer)
+    {
+        line_buffer = malloc(len * sizeof(PDC_EMSCRIPTEN_CELL));
+        line_buffer_size = len;
+    }
+    else if (line_buffer_size < len)
+    {
+        line_buffer = realloc(line_buffer, len * sizeof(PDC_EMSCRIPTEN_CELL));
+        line_buffer_size = len;
+    }
+
     attr_t sysattrs = SP->termattrs;
 
-    for (int i = 0; i < len; ++i) {
+    for (int i = 0; i < len; ++i)
+    {
         bool blink = FALSE;
         bool bold = FALSE;
         bool italic = FALSE;
         bool underline = FALSE;
-        
+
         chtype codePoint = srcp[i] & A_CHARTEXT;
 
         short background = COLOR_BLACK;
@@ -41,7 +55,7 @@ void PDC_transform_line(int lineno, int x, int len, const chtype *srcp)
 
         if (srcp[i] & A_ALTCHARSET && !(srcp[i] & 0xff80))
             codePoint = acs_map[codePoint & 0x7f];
-        
+
         pair_content(PAIR_NUMBER(srcp[i]), &color, &background);
 
         if ((srcp[i] & A_BLINK) && !(sysattrs & A_BLINK))
@@ -49,7 +63,7 @@ void PDC_transform_line(int lineno, int x, int len, const chtype *srcp)
 
         if ((srcp[i] & A_BLINK) && (sysattrs & A_BLINK))
             blink = TRUE;
-        
+
         if ((srcp[i] & A_BOLD) && !(sysattrs & A_BOLD))
             color |= 8;
 
@@ -58,7 +72,7 @@ void PDC_transform_line(int lineno, int x, int len, const chtype *srcp)
 
         if ((srcp[i] & A_ITALIC) && (sysattrs & A_ITALIC))
             italic = TRUE;
-        
+
         if ((srcp[i] & A_REVERSE) && (sysattrs & A_REVERSE))
         {
             short temp = color;
@@ -70,10 +84,16 @@ void PDC_transform_line(int lineno, int x, int len, const chtype *srcp)
         if ((srcp[i] & A_UNDERLINE) && (sysattrs & A_UNDERLINE))
             underline = TRUE;
 
-        EM_ASM(PDCurses.setCell($0, $1, $2, $3, $4, $5, $6, $7, $8), lineno, x + i, codePoint, color, background, blink, bold, underline, italic);
+        line_buffer[i].code_point = codePoint;
+        line_buffer[i].color = color;
+        line_buffer[i].background = background;
+        line_buffer[i].attrs = (blink ? 1 : 0) | (bold ? 2 : 0) | (italic ? 4 : 0) | (underline ? 8 : 0);
     }
+
+    EM_ASM({ PDCurses.PDC_transform_line(HEAPU32, $0 >>> 2, $1, $2, $3); }, line_buffer, lineno, x, len);
 }
 
-int PDC_wcwidth(wchar_t ch) {
+int PDC_wcwidth(wchar_t ch)
+{
     return EM_ASM_INT(return PDCurses.PDC_wcwidth($0), ch);
 }
--- emscripten/pdcemscripten.h
+++ emscripten/pdcemscripten.h
@@ -5,15 +5,24 @@
 
 #include <curspriv.h>
 #include <emscripten.h>
+#include <stdint.h>
 
 #define PDC_EMSCRIPTEN_MAXCOL 256
 
-typedef struct 
+typedef struct
 {
   short r;
   short g;
   short b;
 } PDC_EMSCRIPTEN_COLOR;
+
+typedef struct
+{
+  uint32_t code_point;
+  uint32_t color;
+  uint32_t background;
+  uint32_t attrs;
+} PDC_EMSCRIPTEN_CELL;
 
 extern PDC_EMSCRIPTEN_COLOR pdc_color[PDC_EMSCRIPTEN_MAXCOL];
 
